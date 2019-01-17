0c9c5bc7c8f0466829b1b60fbca3b677b61e2ba0
BaoCai <46781181+BaoCaiH@users.noreply.github.com>
2019-01-17 15:38:12 +0200

    add function print out

--- Python/2019_01_16_Problem_of_check_sum.py
+++ Python/2019_01_16_Problem_of_check_sum.py
@@ -7,30 +7,37 @@
 # 
 # For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
-# In[8]:
+# In[12]:
 
 
 def sum_any_two_numbers_in_list_equal(list, check_total):
     for i in range(len(list)):
         for j in range(i+1, len(list)):
             if (list[i] + list[j]) == check_total:
+                print('The sum of number {} at position {} and number {} at position {} equal {}'.                      format(list[i], i, list[j], j, check_total))
                 return True
+    print('There is no sum of two number equal {}'.format(check_total))
     return False
 
 
-# In[11]:
+# In[3]:
 
 
 l = [10, 15, 3, 7, 3, 7, 9, 1, 10]
 k = 14
+
+
+# In[4]:
+
 
 print('The list is ', end = '')
 print(l)
 print('The check number is ', end = '')
 print(k)
 
-# In[12]:
 
-print('Is there sum of any two number in the list equal to the check number ')
+# In[13]:
+
+
 print(sum_any_two_numbers_in_list_equal(l, k))
 
